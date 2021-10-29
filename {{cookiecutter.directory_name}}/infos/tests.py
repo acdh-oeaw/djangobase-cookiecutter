ceacf1ba12845186fc53b3f55e225d9a70d241f0
csae8092 <peter.andorfer@oeaw.ac.at>
2021-10-29 12:38:02 +0200

    test gh-wf and tests for infos

--- {{cookiecutter.directory_name}}/infos/tests.py
+++ {{cookiecutter.directory_name}}/infos/tests.py
@@ -1,3 +1,41 @@
-# from django.test import TestCase
+from django.apps import apps
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+MODELS = list(apps.all_models['archiv'].values())
+
+client = Client()
+USER = {
+    "username": "temporary1",
+    "password": "temporary1"
+}
+
+
+class InfosTest(TestCase):
+    fixtures = ['dump.json']
+
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user(**USER)
+
+    def test_002_listviews(self):
+        for x in MODELS:
+            try:
+                url = x.get_listview_url()
+            except AttributeError:
+                url = False
+            if url:
+                response = client.get(url)
+                self.assertEqual(response.status_code, 200)
+
+    def test_003_detailviews(self):
+        for x in MODELS:
+            item = x.objects.first()
+            try:
+                url = item.get_absolute_url()
+            except AttributeError:
+                url = False
+            if url:
+                response = client.get(url, {'pk': item.id})
+                self.assertEqual(response.status_code, 200)
