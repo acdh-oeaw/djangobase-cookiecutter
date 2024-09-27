6650c4b8ac49607af33cab963352dfa749f56ef6
csae8092 <p.andorfer@gmail.com>
2024-09-27 16:33:20 +0200

    fix for #9

--- {{cookiecutter.directory_name}}/djangobaseproject/settings.py
+++ {{cookiecutter.directory_name}}/djangobaseproject/settings.py
@@ -80,10 +80,10 @@ ROOT_URLCONF = "djangobaseproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
         "DIRS": [
             os.path.join(BASE_DIR, "templates"),
         ],
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
